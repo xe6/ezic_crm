16752cfe0adf84113b194b339a1540c9791441d7
Assstarte <admin@ezic.io>
2019-04-18 13:24:26 +0300

    Added Login Modal

--- app/src/components/stateful/Auth.jsx
+++ app/src/components/stateful/Auth.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { MDBBtn, MDBModal, MDBModalHeader, MDBModalBody } from "mdbreact";
 import RegisterForm from "../stateless/RegisterForm";
+import LoginForm from "../stateless/LoginForm";
 
 class Auth extends Component {
   constructor(props) {
@@ -36,12 +37,31 @@ class Auth extends Component {
 
         {/* MODALS */}
         <MDBModal
+          toggle={this.toggle("Login")}
+          isOpen={this.state.modalLogin}
+          size="lg"
+          position="center"
+        >
+          <MDBModalHeader>Login</MDBModalHeader>
+          <MDBModalBody
+            className="text-center"
+            style={{ background: "#000", color: "#fff" }}
+          >
+            <LoginForm submit={this.handleLogin} />
+
+            <MDBBtn color="secondary" onClick={this.toggle("Login")}>
+              Close
+            </MDBBtn>
+          </MDBModalBody>
+        </MDBModal>
+        
+        <MDBModal
           toggle={this.toggle("Register")}
           isOpen={this.state.modalRegister}
           size="lg"
           position="center"
         >
-          <MDBModalHeader>Login</MDBModalHeader>
+          <MDBModalHeader>Register</MDBModalHeader>
           <MDBModalBody
             className="text-center"
             style={{ background: "#000", color: "#fff" }}
@@ -58,6 +78,7 @@ class Auth extends Component {
   }
 
   handleLogin(e) {
+    e.preventDefault();
     console.log("Login clicked");
   }
 
