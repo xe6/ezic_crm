668f5c610c9e58829eb2e3fd8002b5fe555640d0
Assstarte <admin@ezic.io>
2019-04-20 17:43:42 +0300

    Refactored DB migration files naming convention

--- db/models/user.js
+++ db/models/user.js
@@ -1,12 +1,18 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    login: DataTypes.STRING,
-    password: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      login: DataTypes.STRING,
+      password: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      email: DataTypes.STRING
+    },
+    {
+      timestamps: true
+    }
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
