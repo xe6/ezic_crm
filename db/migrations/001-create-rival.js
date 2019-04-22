edba6f291b817d0a25cda984319b40a59bbf0b4b
igorekch1 <igor.chernyaev1999@gmail.com>
2019-04-22 23:36:30 +0300

    migratition of rival updated

--- db/migrations/001-create-rival.js
+++ db/migrations/001-create-rival.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Courses', {
+    return queryInterface.createTable('Rivals', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Courses');
+    return queryInterface.dropTable('Rivals');
   }
 };
