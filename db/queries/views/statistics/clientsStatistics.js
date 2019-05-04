3e0f730acfec7ae4e90d7c88ee82138fbf5f7a8b
igorekch1 <igor.chernyaev1999@gmail.com>
2019-05-05 02:39:59 +0300

    implemented extra views to solve the issue w/ subqueries

--- db/queries/views/statistics/clientsStatistics.js
+++ db/queries/views/statistics/clientsStatistics.js
@@ -1,39 +1,18 @@
 // Quantity of clients who enrolled, has gone or now studying for the last 6 month
 const clientsStatiscs = `
     create or replace view \`Clients_statistics\` as
-        select ifnull(t.q, 0) as clients_quantity, ifnull(t.status, 'active') as status, month
-        from (select count(*) as q, status,
-        MONTHNAME(STR_TO_DATE(EXTRACT(MONTH from clients.createdAt) , '%m')) as month_name
-        from Clients
-        where (createdAt BETWEEN (
-        SELECT DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
-        ) AND CURDATE())
-        and Clients.status='active'
-        group by status, month_name ) as t right join Months on t.month_name = Months.month
+        SELECT IFNULL(QUANTITY,0) as quantity, IFNULL(STATUS, 'active') as STATUS, MONTH
+        FROM sumActiveClients RIGHT JOIN Months ON sumActiveClients.month_name = Months.month
 
         UNION ALL
 
-        select ifnull(t.q, 0) as clients_quantity, ifnull(t.status, 'enrolled') as status, month
-        from (select count(*) as q, status,
-        MONTHNAME(STR_TO_DATE(EXTRACT(MONTH from Clients.createdAt) , '%m')) as month_name
-        from Clients
-        where (createdAt BETWEEN (
-        SELECT DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
-        ) AND CURDATE())
-        and Clients.status='enrolled'
-        group by status, month_name ) as t right join Months on t.month_name = Months.month
+        SELECT IFNULL(QUANTITY,0), IFNULL(STATUS, 'active') as STATUS, MONTH
+        FROM sumEnrolledClients RIGHT JOIN Months ON sumEnrolledClients.month_name = Months.month
 
         UNION ALL
 
-        select ifnull(t.q, 0) as clients_quantity, ifnull(t.status, 'gone') as status, month
-        from (select count(*) as q, status,
-        MONTHNAME(STR_TO_DATE(EXTRACT(MONTH from Clients.createdAt) , '%m')) as month_name
-        from Clients
-        where (createdAt BETWEEN (
-        SELECT DATE_SUB(CURDATE(), INTERVAL 6 MONTH)
-        ) AND CURDATE())
-        and Clients.status='gone'
-        group by status, month_name ) as t right join Months on t.month_name = Months.month;
+        SELECT IFNULL(QUANTITY,0), IFNULL(STATUS, 'active') as STATUS, MONTH
+        FROM sumGoneClients RIGHT JOIN Months ON sumGoneClients.month_name = Months.month
 `;
 
 module.exports = clientsStatiscs;
