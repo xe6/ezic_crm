bf7cf23b1d28fd18944c61bb6b1143eb45f88eb3
Assstarte <admin@ezic.io>
2019-05-05 23:52:07 +0300

    Fixed mapping data to charts in BarChart.jsx

--- app/src/components/stateless/statistics/BarChart.jsx
+++ app/src/components/stateless/statistics/BarChart.jsx
@@ -2,47 +2,55 @@ import React from 'react';
 import { MDBCol, MDBCard, MDBCardHeader, MDBCardBody, MDBRow } from 'mdbreact';
 import { Bar } from 'react-chartjs-2';
 
-const BarChart = ({data, period}) => {
-    const dataBar = {
+const BarChart = ({ data, period }) => {
+
+    let activeQ = [];
+    let enrolledQ = [];
+    let goneQ = [];
+
+    if (data) {
+        data.map((item) => {
+            switch (item.status) {
+                case "active":
+                    return activeQ.push(item);
+                case "enrolled":
+                    return enrolledQ.push(item);
+                case "gone":
+                    return goneQ.push(item);
+                default:
+                    return null
+            }
+        });
+    } else return <h1>Loading...</h1>
+
+    console.dir(activeQ);
+
+
+    let dataBar = {
         labels: period ? period.map(item => item.month) : [],
         datasets: [
-        {   
-            label: 'active',
-            data: data ? 
-                data.map(item => {
-                    if (item.status === "active") {
-                        console.log(item)
-                        return item.quantity;
-                    } else return null;
-                })
-            : [],
-            backgroundColor: 'rgba(245, 74, 85, 0.5)',
-            borderWidth: 1
-        }, {
-            label: 'enrolled',
-            data: data ? 
-                data.map(item => {
-                    if (item.status === "enrolled") {
-                        console.log(item.quantity)
-                        return item.quantity;
-                    } else return null;
-                })
-            : [],
-            backgroundColor: 'rgba(90, 173, 246, 0.5)',
-            borderWidth: 1
-        }, {
-            label: 'gone',
-            data: data ? 
-                data.map(item => {
-                    if (item.status === "gone") {
-                        console.log(item)
-                        return item.quantity;
-                    } else return null;
-                })
-            : [],
-            backgroundColor: 'rgba(245, 192, 50, 0.5)',
-            borderWidth: 1
-        }
+            {
+                label: 'active',
+                data: activeQ.map((item) => {
+                    return item.quantity
+                }),
+                backgroundColor: 'rgba(245, 74, 85, 0.5)',
+                borderWidth: 1
+            }, {
+                label: 'enrolled',
+                data: enrolledQ.map((item) => {
+                    return item.quantity
+                }),
+                backgroundColor: 'rgba(90, 173, 246, 0.5)',
+                borderWidth: 1
+            }, {
+                label: 'gone',
+                data: goneQ.map((item) => {
+                    return item.quantity
+                }),
+                backgroundColor: 'rgba(245, 192, 50, 0.5)',
+                borderWidth: 1
+            }
         ]
     };
 
@@ -50,28 +58,28 @@ const BarChart = ({data, period}) => {
         responsive: true,
         maintainAspectRatio: false,
         scales: {
-        xAxes: [{
-            barPercentage: 1,
-            gridLines: {
-            display: true,
-            color: 'rgba(0, 0, 0, 0.1)'
-            }
-        }],
-        yAxes: [{
-            gridLines: {
-            display: true,
-            color: 'rgba(0, 0, 0, 0.1)'
-            },
-            ticks: {
-            beginAtZero: true
-            }
-        }]
+            xAxes: [{
+                barPercentage: 1,
+                gridLines: {
+                    display: true,
+                    color: 'rgba(0, 0, 0, 0.1)'
+                }
+            }],
+            yAxes: [{
+                gridLines: {
+                    display: true,
+                    color: 'rgba(0, 0, 0, 0.1)'
+                },
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
         }
     }
-    
+
     return (
         <MDBRow className="mb-4">
-            <MDBCol md="12"className="mb-4">
+            <MDBCol md="12" className="mb-4">
                 <MDBCard className="mb-4">
                     <MDBCardHeader>Customer statistics</MDBCardHeader>
                     <MDBCardBody>
