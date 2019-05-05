9e4a713c459202f045d607848abe749cf24d23b3
Assstarte <admin@ezic.io>
2019-05-05 20:48:24 +0300

    Fixed letter case for Months table

--- db/queries/views/statistics/dealsStatistics.js
+++ db/queries/views/statistics/dealsStatistics.js
@@ -2,12 +2,12 @@
 const dealsStatistics = `
     create or replace view \`Deals_statistics\` as
         SELECT IFNULL(stage, 'new') AS stage, IFNULL(quantity, 0), MONTH FROM Sum_new_deals
-        RIGHT JOIN months ON Sum_new_deals.month_name = Months.month
+        RIGHT JOIN Months ON Sum_new_deals.month_name = Months.month
 
         UNION ALL
 
         SELECT IFNULL(stage, 'new') AS stage, IFNULL(quantity, 0), MONTH FROM Sum_success_deals
-        RIGHT JOIN months ON Sum_success_deals.month_name = Months.month;
+        RIGHT JOIN Months ON Sum_success_deals.month_name = Months.month;
 `;
 
 module.exports = dealsStatistics;
